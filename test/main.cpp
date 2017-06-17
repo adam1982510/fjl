7dc504c8d94a71218c2e74eca5dbf9000ea83db4
冯佳良 <fjl@fjl.local>
2017-06-17 14:59:15 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 冯佳良 on 2017/6/17.
+//  Copyright © 2017年 冯佳良. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
